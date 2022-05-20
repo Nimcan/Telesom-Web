12782ba91bc9245f1829025356a518b2bc37a9bd
Nimcan <nbashiir26@gmail.com>
2022-05-20 21:14:27 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='font-bold text-3xl underline'>hello waryaadaha
+  
+       </h1>
+    </div>
+  );
+}
+
+export default App;
