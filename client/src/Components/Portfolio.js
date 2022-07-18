43cd5ce8ee5f5d4638e8bb029433cf73edaf912a
Nimcan <nbashiir26@gmail.com>
2022-07-18 21:52:44 +0300

    portofolio

--- client/src/Components/Portfolio.js
+++ client/src/Components/Portfolio.js
@@ -7,7 +7,7 @@ function Portfolio() {
     <div className='contain '>
         <h1 className='text-center text-3xl mb-8'>Telesom Group Portofolio</h1>
 
-        <div className='grid md:grid-cols-3 px-4 '>
+        <div className='grid md:grid-cols-3 px-4 gap-4'>
             <div className='flex flex-col justify-between border border-gray-300'>
                 <div className=''>
                     <img src='./images/kaabsan.png' alt='' className='m-auto' />
@@ -21,10 +21,70 @@ function Portfolio() {
                 </div>
             </div>
 
-           
+            <div className='flex flex-col justify-between border border-gray-300'>
+                <div className=''>
+                    <img src='./images/kaabsan.png' alt='' className='m-auto' />
+                </div>
+                <div className=''>
+                    <h1 className='text-xl px-2 pt-2 text-center'>KAABSAN</h1>
+                    <p className='p-3 text-center'>Kaabsan is the first and the only producer of quality-controlled ready-mixed concrete by
+                         computerized batching systems ensuring consistency
+                         throughout...
+                    </p>
+                </div>
+            </div>
 
+            <div className='flex flex-col justify-between border border-gray-300'>
+                <div className=''>
+                    <img src='./images/kaabsan.png' alt='' className='m-auto' />
+                </div>
+                <div className=''>
+                    <h1 className='text-xl px-2 pt-2 text-center'>KAABSAN</h1>
+                    <p className='p-3 text-center'>Kaabsan is the first and the only producer of quality-controlled ready-mixed concrete by
+                         computerized batching systems ensuring consistency
+                         throughout...
+                    </p>
+                </div>
+            </div>
 
+            <div className='flex flex-col justify-between border border-gray-300'>
+                <div className=''>
+                    <img src='./images/kaabsan.png' alt='' className='m-auto' />
+                </div>
+                <div className=''>
+                    <h1 className='text-xl px-2 pt-2 text-center'>KAABSAN</h1>
+                    <p className='p-3 text-center'>Kaabsan is the first and the only producer of quality-controlled ready-mixed concrete by
+                         computerized batching systems ensuring consistency
+                         throughout...
+                    </p>
+                </div>
+            </div>
 
+            <div className='flex flex-col justify-between border border-gray-300'>
+                <div className=''>
+                    <img src='./images/kaabsan.png' alt='' className='m-auto' />
+                </div>
+                <div className=''>
+                    <h1 className='text-xl px-2 pt-2 text-center'>KAABSAN</h1>
+                    <p className='p-3 text-center'>Kaabsan is the first and the only producer of quality-controlled ready-mixed concrete by
+                         computerized batching systems ensuring consistency
+                         throughout...
+                    </p>
+                </div>
+            </div>
+
+            <div className='flex flex-col justify-between border border-gray-300'>
+                <div className=''>
+                    <img src='./images/kaabsan.png' alt='' className='m-auto' />
+                </div>
+                <div className=''>
+                    <h1 className='text-xl px-2 pt-2 text-center'>KAABSAN</h1>
+                    <p className='p-3 text-center'>Kaabsan is the first and the only producer of quality-controlled ready-mixed concrete by
+                         computerized batching systems ensuring consistency
+                         throughout...
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
     </div>
